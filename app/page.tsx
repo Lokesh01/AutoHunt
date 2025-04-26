342c53ebbd02d51184b6c68f25a6ee2ab09fc7a3
Lokesh <lokeshnegi649@gmail.com>
2025-04-26 21:09:58 +0530

    add dummy data and dark theme for header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-
 export default function Home() {
   return (
-<div>car market place</div>
+    <div className="flex flex-col pt-20">
+      {/* Hero Section */}
+      <section className="relative py-16 md:py-28 dotted-background">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="mb-8">
+            <h1 className="glow-effect text-5xl md:text-8xl mb-4 gradient-title">
+              Find your dream Ride with AutoHunt
+            </h1>
+            <p className="text-xl text-black dark:text-white mb-8 max-w-2xl mx-auto">
+              Discover & Test Drive Your Perfect Car with AI-Powered Search.{" "}
+            </p>
+          </div>
+
+          <p>replace with search comp</p>
+        </div>
+      </section>
+    </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ const Header = ({ isAdminPage = false }: HeaderProps) => {
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b dark:bg-gray-900/80 dark:border-gray-700">
       <nav className="mx-auto px-4 py-1 flex items-center justify-between">
-        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex glow-effect">
           <Image
             src={"/shop-logo/logo-transparent.png"}
             alt="Vehicle Logo"
